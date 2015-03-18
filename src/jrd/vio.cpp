b0b9431f7d5ae577f3cadc1dbebed9e68baccdc2
hvlad <hvlad@users.sourceforge.net>
2015-03-18 17:19:43 +0000

    Fixed bug CORE-4713 : "BLOB not found" error at rollback after insert into table with expression index

--- src/jrd/vio.cpp
+++ src/jrd/vio.cpp
@@ -400,8 +400,8 @@ void VIO_backout(thread_db* tdbb, record_param* rpb, const jrd_tra* transaction)
 			DPM_backout_mark(tdbb, rpb, transaction);
 
 			RecordStack empty_staying;
-			BLB_garbage_collect(tdbb, going, empty_staying, rpb->rpb_page, relation);
 			IDX_garbage_collect(tdbb, rpb, going, empty_staying);
+			BLB_garbage_collect(tdbb, going, empty_staying, rpb->rpb_page, relation);
 			going.pop();
 
 			if (!DPM_get(tdbb, rpb, LCK_write))
@@ -435,8 +435,8 @@ void VIO_backout(thread_db* tdbb, record_param* rpb, const jrd_tra* transaction)
 
 		rpb->rpb_prior = NULL;
 		list_staying(tdbb, rpb, staying);
-		BLB_garbage_collect(tdbb, going, staying, rpb->rpb_page, relation);
 		IDX_garbage_collect(tdbb, rpb, going, staying);
+		BLB_garbage_collect(tdbb, going, staying, rpb->rpb_page, relation);
 
 		if (going.hasData())
 		{
@@ -3944,8 +3944,8 @@ static void garbage_collect(thread_db* tdbb,
 			JRD_reschedule(tdbb, 0, true);
 	}
 
-	BLB_garbage_collect(tdbb, going, staying, prior_page, rpb->rpb_relation);
 	IDX_garbage_collect(tdbb, rpb, going, staying);
+	BLB_garbage_collect(tdbb, going, staying, prior_page, rpb->rpb_relation);
 
 	clearRecordStack(going);
 }
@@ -3985,8 +3985,8 @@ static void garbage_collect_idx(thread_db* tdbb,
 
 	going.push(old_data ? old_data : org_rpb->rpb_record);
 
-	BLB_garbage_collect(tdbb, going, staying, org_rpb->rpb_page, org_rpb->rpb_relation);
 	IDX_garbage_collect(tdbb, org_rpb, going, staying);
+	BLB_garbage_collect(tdbb, going, staying, org_rpb->rpb_page, org_rpb->rpb_relation);
 
 	going.pop();
 
@@ -5300,8 +5300,8 @@ static void update_in_place(thread_db* tdbb,
 		RecordStack going;
 		going.push(org_rpb->rpb_record);
 
-		BLB_garbage_collect(tdbb, going, staying, org_rpb->rpb_page, relation);
 		IDX_garbage_collect(tdbb, org_rpb, going, staying);
+		BLB_garbage_collect(tdbb, going, staying, org_rpb->rpb_page, relation);
 
 		staying.pop();
 		clearRecordStack(staying);
