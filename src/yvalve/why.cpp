9bf96ddf6703befc859040a3679ad622ee4a6759
alexpeshkoff <alexpeshkoff@users.sourceforge.net>
2015-05-18 16:46:20 +0000

    Fixed CORE-4795: fbclient executes callback function twice

--- src/yvalve/why.cpp
+++ src/yvalve/why.cpp
@@ -2880,16 +2880,16 @@ namespace
 		// IEventCallback implementation
 		virtual void eventCallbackFunction(unsigned int length, const UCHAR* list)
 		{
-			try
-			{
-				MutexLockGuard g(mtx, FB_FUNCTION);
-				ast(arg, length, list);
-			}
-			catch (const Firebird::Exception&)
-			{ }
-
 			if (events)
 			{
+				try
+				{
+					MutexLockGuard g(mtx, FB_FUNCTION);
+					ast(arg, length, list);
+				}
+				catch (const Firebird::Exception&)
+				{ }
+
 				events->autoReleased = true;
 				YEvents* tmp = events;
 				events = NULL;
