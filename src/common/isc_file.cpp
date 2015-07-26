5c96bd5401e782cd7299a280d7e39d332e253a29
dimitr <dimitr@users.sourceforge.net>
2015-07-26 07:50:58 +0000

    Fixed hostless INET/WNET connections on Linux.

--- src/common/isc_file.cpp
+++ src/common/isc_file.cpp
@@ -344,7 +344,7 @@ bool ISC_analyze_protocol(const char* protocol, tstring& expanded_name, tstring&
 	if (separator) // this implies node name is expected!
 	{
 		size p = expanded_name.find_first_of('/');
-		if (p != npos)
+		if (p != 0 && p != npos)
 		{
 			node_name = expanded_name.substr(0, p);
 			expanded_name.erase(0, node_name.length() + 1);
