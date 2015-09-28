659e3e2ca5bced6eb98cf3d6bb337e33b10935da
paulbeach <paulbeach@users.sourceforge.net>
2015-09-28 13:27:41 +0000

    Fix an irritating warning when building packages

--- src/jrd/license.h
+++ src/jrd/license.h
@@ -125,7 +125,7 @@
 #if defined(__ppc__) || defined(__ppc64__)
 #define FB_PLATFORM     "UP"	/* Darwin/PowerPC */
 #endif
-#endif	// DARWIN
+#endif	
 
 #ifndef FB_VERSION
 #define FB_VERSION      FB_PLATFORM "-" FB_BUILD_TYPE FB_MAJOR_VER "." FB_MINOR_VER "." FB_REV_NO "." FB_BUILD_NO " " FB_BUILD_SUFFIX
