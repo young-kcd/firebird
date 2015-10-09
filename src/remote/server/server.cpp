eb4a6c620c2f76f96ecfc940c959aa55a5272690
asfernandes <asfernandes@users.sourceforge.net>
2015-10-09 14:47:52 +0000

    Fix debug code.

--- src/remote/server/server.cpp
+++ src/remote/server/server.cpp
@@ -5499,7 +5499,7 @@ void rem_port::start_crypt(P_CRYPT * crypt, PACKET* sendL)
 		port_crypt_complete = true;
 
 		send_response(sendL, 0, 0, &st, false);
-		WIRECRYPT_DEBUG(fprintf(stderr, "Installed cipher %s key %s\n", cp.name(), key->type));
+		WIRECRYPT_DEBUG(fprintf(stderr, "Installed cipher %s\n", cp.name()));
 	}
 	catch (const Exception& ex)
 	{
