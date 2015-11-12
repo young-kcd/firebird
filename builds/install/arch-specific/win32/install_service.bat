cc97f27845119b425a2d2284ceac2a31b7224872
paul_reeves <paul_reeves@users.sourceforge.net>
2015-11-12 07:19:37 +0000

    Remove comments left in by error while testing.

--- builds/install/arch-specific/win32/install_service.bat
+++ builds/install/arch-specific/win32/install_service.bat
@@ -9,8 +9,8 @@ if not "%1"=="" (
 set INSTALL_SERVICE=%INSTALL_SERVICE% -n %1
 set START_SERVICE=%START_SERVICE% -n %1
 )
-::instsvc %INSTALL_SERVICE%
-::instsvc %START_SERVICE%
+instsvc %INSTALL_SERVICE%
+instsvc %START_SERVICE%
 endlocal
 @echo.
 @echo Firebird installed and started as a service using 
