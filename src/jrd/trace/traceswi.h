1c920ac5780792fecb1296c26dfa3cc077117ecc
Alexander Peshkov <peshkoff@mail.ru>
2016-06-15 19:16:29 +0300

    Fixed CORE-5278: A number of SPB parameters fail in FB 3 & 4

--- src/jrd/trace/traceswi.h
+++ src/jrd/trace/traceswi.h
@@ -45,32 +45,32 @@ const int IN_SW_TRACE_VERSION		= 14;
 // list of possible actions (services) for use with trace services
 static const struct Switches::in_sw_tab_t trace_action_in_sw_table [] =
 {
-	{IN_SW_TRACE_LIST,		isc_action_svc_trace_list,		"LIST", 	0, 0, 0, false,	0,	1, NULL},
-	{IN_SW_TRACE_RESUME,	isc_action_svc_trace_resume,	"RESUME", 	0, 0, 0, false,	0,	1, NULL},
-	{IN_SW_TRACE_STOP,		isc_action_svc_trace_stop,		"STOP", 	0, 0, 0, false,	0,	3, NULL},
-	{IN_SW_TRACE_START,		isc_action_svc_trace_start,		"START",	0, 0, 0, false,	0,	3, NULL},
-	{IN_SW_TRACE_SUSPEND,	isc_action_svc_trace_suspend,	"SUSPEND",	0, 0, 0, false,	0,	2, NULL},
-	{IN_SW_TRACE_VERSION,	0,								"Z",		0, 0, 0, false, 0,	1, NULL},
-	{0,						0,								NULL,		0, 0, 0, false, 0,	0, NULL}	// End of List
+	{IN_SW_TRACE_LIST,		isc_action_svc_trace_list,		"LIST", 	0, 0, 0, false,	false,	0,	1, NULL},
+	{IN_SW_TRACE_RESUME,	isc_action_svc_trace_resume,	"RESUME", 	0, 0, 0, false,	false,	0,	1, NULL},
+	{IN_SW_TRACE_STOP,		isc_action_svc_trace_stop,		"STOP", 	0, 0, 0, false,	false,	0,	3, NULL},
+	{IN_SW_TRACE_START,		isc_action_svc_trace_start,		"START",	0, 0, 0, false,	false,	0,	3, NULL},
+	{IN_SW_TRACE_SUSPEND,	isc_action_svc_trace_suspend,	"SUSPEND",	0, 0, 0, false,	false,	0,	2, NULL},
+	{IN_SW_TRACE_VERSION,	0,								"Z",		0, 0, 0, false,	false, 0,	1, NULL},
+	{0,						0,								NULL,		0, 0, 0, false,	false, 0,	0, NULL}	// End of List
 };
 
 
 // list of actions (services) parameters
 static const struct Switches::in_sw_tab_t trace_option_in_sw_table [] =
 {
-	{IN_SW_TRACE_CONFIG,	isc_spb_trc_cfg,	"CONFIG", 	0, 0, 0, false,	0,	1, NULL},
-	{IN_SW_TRACE_ID,		isc_spb_trc_id,		"ID",		0, 0, 0, false,	0,	1, NULL},
-	{IN_SW_TRACE_NAME,		isc_spb_trc_name,	"NAME", 	0, 0, 0, false,	0,	1, NULL},
-	{0,						0,					NULL,		0, 0, 0, false, 0,	0, NULL}	// End of List
+	{IN_SW_TRACE_CONFIG,	isc_spb_trc_cfg,	"CONFIG", 	0, 0, 0, false,	false,	0,	1, NULL},
+	{IN_SW_TRACE_ID,		isc_spb_trc_id,		"ID",		0, 0, 0, false,	false,	0,	1, NULL},
+	{IN_SW_TRACE_NAME,		isc_spb_trc_name,	"NAME", 	0, 0, 0, false,	false,	0,	1, NULL},
+	{0,						0,					NULL,		0, 0, 0, false,	false, 0,	0, NULL}	// End of List
 };
 
 // authentication switches, common for all utils (services)
 static const struct Switches::in_sw_tab_t trace_auth_in_sw_table [] =
 {
-	{IN_SW_TRACE_FETCH_PWD,		0,	"FETCH_PASSWORD",		0, 0, 0, false,	0,	2, NULL},
-	{IN_SW_TRACE_PASSWORD,		0,	PASSWORD_SWITCH,		0, 0, 0, false,	0,	1, NULL},
-	{IN_SW_TRACE_SERVICE_NAME,	0,	"SERVICE",				0, 0, 0, false,	0,	2, NULL},
-	{IN_SW_TRACE_TRUSTED_AUTH,	0,	"TRUSTED",				0, 0, 0, false,	0,	1, NULL},
-	{IN_SW_TRACE_USERNAME,		0,	USERNAME_SWITCH,		0, 0, 0, false,	0,	1, NULL},
-	{0,							0,	NULL,					0, 0, 0, false, 0,	0, NULL}	// End of List
+	{IN_SW_TRACE_FETCH_PWD,		0,	"FETCH_PASSWORD",		0, 0, 0, false,	false,	0,	2, NULL},
+	{IN_SW_TRACE_PASSWORD,		0,	PASSWORD_SWITCH,		0, 0, 0, false,	false,	0,	1, NULL},
+	{IN_SW_TRACE_SERVICE_NAME,	0,	"SERVICE",				0, 0, 0, false,	false,	0,	2, NULL},
+	{IN_SW_TRACE_TRUSTED_AUTH,	0,	"TRUSTED",				0, 0, 0, false,	false,	0,	1, NULL},
+	{IN_SW_TRACE_USERNAME,		0,	USERNAME_SWITCH,		0, 0, 0, false,	false,	0,	1, NULL},
+	{0,							0,	NULL,					0, 0, 0, false,	false, 0,	0, NULL}	// End of List
 };
