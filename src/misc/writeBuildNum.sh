f308cc273907a82c1d3caad98097d2f066c3a98f
Dmitry Yemanov <dyemanov@users.noreply.github.com>
2016-07-28 15:30:27 +0300

    Bump the version number.

--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -8,7 +8,7 @@ BuildVersion="$Id: writeBuildNum.sh,v 1.25898.2.19 2010/05/28 14:38:22 alexpeshk
 BuildType=V
 MajorVer=2
 MinorVer=5
-RevNo=6
+RevNo=7
 BuildNum=27023
 
 if [ "$SPECIAL_BUILD_SUFFIX" = "" ]; then
