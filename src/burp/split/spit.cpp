33198c6db52a104ab3c97bcd03dc6fbda14b03fc
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2016-09-02 08:24:22 -0300

    Use nullptr keyword and fix a related problem caught by MSVC.

--- src/burp/split/spit.cpp
+++ src/burp/split/spit.cpp
@@ -125,9 +125,9 @@ enum gsplit_option
 
 const static Switches::in_sw_tab_t spit_in_sw_table[] =
 {
-	{IN_SW_SPIT_SP,	0,	"SPLIT_BK_FILE", 0, 0, 0, false, 0, 0, NULL},
-	{IN_SW_SPIT_JT,	0,	"JOIN_BK_FILE",	 0, 0, 0, false, 0, 0, NULL},
-	{IN_SW_SPIT_0,	0,	NULL,			 0, 0, 0, false, 0, 0, NULL}
+	{IN_SW_SPIT_SP,	0,	"SPLIT_BK_FILE", 0, 0, 0, false, 0, 0, 0},
+	{IN_SW_SPIT_JT,	0,	"JOIN_BK_FILE",	 0, 0, 0, false, 0, 0, 0},
+	{IN_SW_SPIT_0,	0,	NULL,			 0, 0, 0, false, 0, 0, 0}
 };
 
 
