5d3564f0cf8285b049b652e10681521ebe3b1c08
firebirds <>
2016-10-22 00:02:21 +0000

    increment build number

--- src/jrd/build_no.h
+++ src/jrd/build_no.h
@@ -3,16 +3,16 @@
                *** DO NOT EDIT ***
   TO CHANGE ANY INFORMATION IN HERE PLEASE
   EDIT src/misc/writeBuildNum.sh
-  FORMAL BUILD NUMBER:420
+  FORMAL BUILD NUMBER:423
 */
 
-#define PRODUCT_VER_STRING "4.0.0.420"
-#define FILE_VER_STRING "WI-T4.0.0.420"
-#define LICENSE_VER_STRING "WI-T4.0.0.420"
-#define FILE_VER_NUMBER 4, 0, 0, 420
+#define PRODUCT_VER_STRING "4.0.0.423"
+#define FILE_VER_STRING "WI-T4.0.0.423"
+#define LICENSE_VER_STRING "WI-T4.0.0.423"
+#define FILE_VER_NUMBER 4, 0, 0, 423
 #define FB_MAJOR_VER "4"
 #define FB_MINOR_VER "0"
 #define FB_REV_NO "0"
-#define FB_BUILD_NO "420"
+#define FB_BUILD_NO "423"
 #define FB_BUILD_TYPE "T"
 #define FB_BUILD_SUFFIX "Firebird 4.0 Unstable"
--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -9,7 +9,7 @@ BuildType=T
 MajorVer=4
 MinorVer=0
 RevNo=0
-BuildNum=420
+BuildNum=423
 
 NowAt=`pwd`
 cd `dirname $0`
