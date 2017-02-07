24d998285dff2803f6edfd8e30a8146e5a332963
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2017-02-07 14:45:43 +0000

    Fixed CORE-5447 - EXECUTE STATEMENT with a statement with "Unexpected end of command" error is reported with wrong column number.

--- src/dsql/Parser.cpp
+++ src/dsql/Parser.cpp
@@ -101,7 +101,7 @@ Parser::Parser(MemoryPool& pool, DsqlCompilerScratch* aScratch, USHORT aClientDi
 	yylexemes = 0;
 
 	lex.start = string;
-	lex.line_start = lex.ptr = string;
+	lex.line_start = lex.last_token = lex.ptr = string;
 	lex.end = string + length;
 	lex.lines = 1;
 	lex.att_charset = characterSet;
