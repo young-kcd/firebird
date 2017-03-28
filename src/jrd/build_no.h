c7e2da7911738d6fc83c6a76428e5d508773ce24
firebirds <>
2017-03-28 00:02:19 +0000

    increment build number

--- src/jrd/build_no.h
+++ src/jrd/build_no.h
@@ -3,16 +3,16 @@
                *** DO NOT EDIT ***
   TO CHANGE ANY INFORMATION IN HERE PLEASE
   EDIT src/misc/writeBuildNum.sh
-  FORMAL BUILD NUMBER:576
+  FORMAL BUILD NUMBER:578
 */
 
-#define PRODUCT_VER_STRING "4.0.0.576"
-#define FILE_VER_STRING "WI-T4.0.0.576"
-#define LICENSE_VER_STRING "WI-T4.0.0.576"
-#define FILE_VER_NUMBER 4, 0, 0, 576
+#define PRODUCT_VER_STRING "4.0.0.578"
+#define FILE_VER_STRING "WI-T4.0.0.578"
+#define LICENSE_VER_STRING "WI-T4.0.0.578"
+#define FILE_VER_NUMBER 4, 0, 0, 578
 #define FB_MAJOR_VER "4"
 #define FB_MINOR_VER "0"
 #define FB_REV_NO "0"
-#define FB_BUILD_NO "576"
+#define FB_BUILD_NO "578"
 #define FB_BUILD_TYPE "T"
 #define FB_BUILD_SUFFIX "Firebird 4.0 Unstable"
