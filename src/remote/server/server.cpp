ca6e9dc7fc3fd3d02df0e1d1d45787b2aa0b32bb
Alex Peshkoff <peshkoff@mail.ru>
2017-08-30 13:55:58 +0300

    Simplify code as suggested by Dmitry

--- src/remote/server/server.cpp
+++ src/remote/server/server.cpp
@@ -2182,22 +2182,16 @@ static void addClumplets(ClumpletWriter* dpb_buffer,
 	if (port->port_address.hasData())
 		address_record.insertString(isc_dpb_addr_endpoint, port->port_address);
 
-	if (port->port_crypt_plugin
+	int flags = 0;
 #ifdef WIRE_COMPRESS_SUPPORT
-						|| port->port_compressed
+	if (port->port_compressed)
+		flags |= isc_dpb_addr_flag_conn_compressed;
 #endif
-									)
-	{
-		int flags = 0;
-#ifdef WIRE_COMPRESS_SUPPORT
-		if (port->port_compressed)
-			flags |= isc_dpb_addr_flag_conn_compressed;
-#endif
-		if (port->port_crypt_plugin)
-			flags |= isc_dpb_addr_flag_conn_encrypted;
+	if (port->port_crypt_plugin)
+		flags |= isc_dpb_addr_flag_conn_encrypted;
 
+	if (flags)
 		address_record.insertInt(isc_dpb_addr_flags, flags);
-	}
 
 	// We always insert remote address descriptor as a first element
 	// of appropriate clumplet so user cannot fake it and engine may somewhat trust it.
