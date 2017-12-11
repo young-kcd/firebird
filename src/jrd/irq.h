3f62ad0e0abc505a2032fd34efbd403a961a5e81
Roman Simakov <roman.simakov@gmail.com>
2017-12-11 08:40:52 +0300

    Fixed CORE-5677: RDB$PAGES is dirty after error after phase 3 of create_relation (#135)

--- src/jrd/irq.h
+++ src/jrd/irq.h
@@ -175,6 +175,7 @@ enum irq_type_t
 	irq_grant18,			// process grant option (filters)
 	irq_grant19,			// process grant option (roles)
 	irq_l_curr_format,		// lookup table's current format
+	irq_c_relation3,		// lookup relation in phase 0 to cleanup
 
 	irq_linger,				// get database linger value
 	irq_dbb_ss_definer,		// get database sql security value
