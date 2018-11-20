077a2a3b7558f5a7c5cc7e1370e51ceed5d5c025
Dmitry Yemanov <dyemanov@users.noreply.github.com>
2018-11-20 19:44:17 +0300

    Attempt to fix CORE-5070, CORE-5795, CORE-5845, CORE-5965 with a single shot

--- src/jrd/Optimizer.h
+++ src/jrd/Optimizer.h
@@ -180,7 +180,8 @@ public:
 	IndexTableScan* getNavigation();
 
 protected:
-	void analyzeNavigation();
+	void analyzeNavigation(const InversionCandidateList& inversions);
+	bool betterInversion(const InversionCandidate* inv1, const InversionCandidate* inv2) const;
 	InversionNode* composeInversion(InversionNode* node1, InversionNode* node2,
 		InversionNode::Type node_type) const;
 	const Firebird::string& getAlias();
@@ -222,7 +223,7 @@ public:
 	bool outerFlag;
 	bool createIndexScanNodes;
 	bool setConjunctionsMatched;
-	IndexScratch* navigationCandidate;
+	InversionCandidate* navigationCandidate;
 };
 
 class IndexRelationship
