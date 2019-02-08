61138cf026131f2e8be3758501d21275778a0019
Paul Reeves <paul_reeves@users.sourceforge.net>
2019-02-08 21:41:56 +0100

    Use correct path to locate vcvarsall.bat when building with VS150COMNTOOOLS.

--- builds/win32/setenvvar.bat
+++ builds/win32/setenvvar.bat
@@ -25,7 +25,7 @@ set FB_PROCESSOR_ARCHITECTURE=%PROCESSOR_ARCHITECTURE%
 
 if DEFINED VS150COMNTOOLS (
 @devenv /? >nul 2>nul
-@if errorlevel 9009 (call "%VS150COMNTOOLS%\..\..\VC\vcvarsall.bat" %FB_PROCESSOR_ARCHITECTURE%) else ( echo    The file: & @echo      "%VS150COMNTOOLS%\..\..\VC\vcvarsall.bat" %FB_PROCESSOR_ARCHITECTURE% & echo    has already been executed.)
+@if errorlevel 9009 (call "%VS150COMNTOOLS%\..\..\VC\Auxiliary\Build\vcvarsall.bat" %FB_PROCESSOR_ARCHITECTURE%) else ( echo    The file: & @echo      "%VS150COMNTOOLS%\..\..\VC\Auxiliary\Build\vcvarsall.bat" %FB_PROCESSOR_ARCHITECTURE% & echo    has already been executed.)
 ) else (
 if DEFINED VS140COMNTOOLS (
 @devenv /? >nul 2>nul
