4280f2b70444d2cbc2aa3973aebf07bdce312a39
Dmitry Yemanov <dyemanov@users.noreply.github.com>
2019-07-20 11:49:22 +0300

    Fixed CORE-6104: incorrect erroris thrown when an embedded user does not have SELECT permissions

--- src/gpre/languages/pas.cpp
+++ src/gpre/languages/pas.cpp
@@ -998,11 +998,18 @@ static void gen_compile( const act* action, int column)
 	else
 		printa(column, "if %s = nil then", request->req_handle);
 
-	align(column + INDENT);
+	column += INDENT;
+	begin(column);
+
+	align(column);
 	fprintf(gpreGlob.out_file, "GDS__COMPILE_REQUEST%s (%s, %s, %s, %d, gds__%d);\n",
 			   (request->req_flags & REQ_exp_hand) ? "" : "2",
 			   status_vector(action), symbol->sym_string, request->req_handle,
 			   request->req_length, request->req_ident);
+
+	set_sqlcode(action, column);
+	ends(column);
+	column -= INDENT;
 
 	// If blobs are present, zero out all of the blob handles.  After this
 	// point, the handles are the user's responsibility
