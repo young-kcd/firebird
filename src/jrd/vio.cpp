4890c3baf3734163c1c102e2c78681889d42d2b8
Dmitry Yemanov <dyemanov@users.noreply.github.com>
2020-02-27 12:34:20 +0300

    Yet another postfix

--- src/jrd/vio.cpp
+++ src/jrd/vio.cpp
@@ -2636,6 +2636,8 @@ void VIO_modify(thread_db* tdbb, record_param* org_rpb, record_param* new_rpb, j
 
 	if (needDfw(tdbb, transaction))
 	{
+		const SLONG nullLinger = 0;
+
 		switch ((RIDS) relation->rel_id)
 		{
 		case rel_segments:
@@ -2678,12 +2680,12 @@ void VIO_modify(thread_db* tdbb, record_param* org_rpb, record_param* new_rpb, j
 		case rel_database:
 			protect_system_table_delupd(tdbb, relation, "UPDATE");
 			check_class(tdbb, transaction, org_rpb, new_rpb, f_dat_class);
-			EVL_field(0, org_rpb->rpb_record, f_dat_linger, &desc1);
-			EVL_field(0, new_rpb->rpb_record, f_dat_linger, &desc2);
+			if (!EVL_field(0, org_rpb->rpb_record, f_dat_linger, &desc1))
+				desc1.makeLong(0, const_cast<SLONG*>(&nullLinger));
+			if (!EVL_field(0, new_rpb->rpb_record, f_dat_linger, &desc2))
+				desc2.makeLong(0, const_cast<SLONG*>(&nullLinger));
 			if (MOV_compare(&desc1, &desc2))
-			{
 				DFW_post_work(transaction, dfw_set_linger, &desc2, 0);
-			}
 			break;
 
 		case rel_relations:
