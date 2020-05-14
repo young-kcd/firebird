986e0a523879f953a97112913167e82c8cc47fb3
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2020-05-14 11:54:16 -0300

    Fix Travis (Ubuntu Xenial) build.
    This reverts commit 07456f37cd9c6bdb983cf3ca045798fa0a60da5d.

--- src/common/classes/FpeControl.h
+++ src/common/classes/FpeControl.h
@@ -222,6 +222,14 @@ inline bool isinf(double x)
 {
 	return (!_finite (x) && !isnan(x));
 }
+#else
+#ifndef isinf
+template <typename F>
+inline bool isinf(F x)
+{
+	return !isnan(x) && isnan(x - x);
+}
+#endif // isinf
 #endif // WIN_NT
 
 namespace Firebird {
