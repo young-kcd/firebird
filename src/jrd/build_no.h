f9e6da1714aeb2d064441dcb53d4aded37b23429
firebirds <>
2020-10-24 00:02:15 +0000

    increment build number

--- src/jrd/build_no.h
+++ src/jrd/build_no.h
@@ -3,16 +3,16 @@
                *** DO NOT EDIT ***
   TO CHANGE ANY INFORMATION IN HERE PLEASE
   EDIT src/misc/writeBuildNum.sh
-  FORMAL BUILD NUMBER:2234
+  FORMAL BUILD NUMBER:2235
 */
 
-#define PRODUCT_VER_STRING "4.0.0.2234"
-#define FILE_VER_STRING "WI-V4.0.0.2234"
-#define LICENSE_VER_STRING "WI-V4.0.0.2234"
-#define FILE_VER_NUMBER 4, 0, 0, 2234
+#define PRODUCT_VER_STRING "4.0.0.2235"
+#define FILE_VER_STRING "WI-V4.0.0.2235"
+#define LICENSE_VER_STRING "WI-V4.0.0.2235"
+#define FILE_VER_NUMBER 4, 0, 0, 2235
 #define FB_MAJOR_VER "4"
 #define FB_MINOR_VER "0"
 #define FB_REV_NO "0"
-#define FB_BUILD_NO "2234"
+#define FB_BUILD_NO "2235"
 #define FB_BUILD_TYPE "V"
 #define FB_BUILD_SUFFIX "Firebird 4.0 Release Candidate 1"
