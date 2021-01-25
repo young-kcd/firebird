b8f4ae73229a20800a866158bd05198c0d0abbf5
hvlad <hvlad@users.sourceforge.net>
2021-01-25 11:46:19 +0200

    Avoid recursive calls in memory pool printing code. It allows to avoid stack overflow when there are huge number of extents allocated.

--- src/common/classes/alloc.cpp
+++ src/common/classes/alloc.cpp
@@ -491,9 +491,6 @@ public:
 			block->print_contents(block->pool == pool, file, used_only, filter_path, filter_len);
 			m += block->getSize();
 		}
-
-		if (next)
-			next->print_contents(file, pool, used_only, filter_path, filter_len);
 	}
 #endif
 
@@ -638,8 +635,6 @@ public:
 		fprintf(file, "Big hunk %p: memory=%p length=%" SIZEFORMAT "\n",
 			this, block, length);
 		block->print_contents(true, file, used_only, filter_path, filter_len);
-		if (next)
-			next->print_contents(file, pool, used_only, filter_path, filter_len);
 	}
 #endif
 
@@ -1703,8 +1698,8 @@ public:
 	void print_contents(FILE* file, MemPool* pool, bool used_only,
 						const char* filter_path, const size_t filter_len) FB_NOTHROW
 	{
-		if (currentExtent)
-			currentExtent->print_contents(file, pool, used_only, filter_path, filter_len);
+		for (Extent* ext = currentExtent; ext; ext = ext->next)
+			ext->print_contents(file, pool, used_only, filter_path, filter_len);
 	}
 #endif
 
