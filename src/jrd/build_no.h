7fbcea81a9f10df557a32144bcb2da9f37166126
firebirds <>
2021-04-08 00:05:14 +0000

    increment build number

--- src/jrd/build_no.h
+++ src/jrd/build_no.h
@@ -3,16 +3,16 @@
                *** DO NOT EDIT ***
   TO CHANGE ANY INFORMATION IN HERE PLEASE
   EDIT src/misc/writeBuildNum.sh
-  FORMAL BUILD NUMBER:2411
+  FORMAL BUILD NUMBER:2416
 */
 
-#define PRODUCT_VER_STRING "4.0.0.2411"
-#define FILE_VER_STRING "WI-V4.0.0.2411"
-#define LICENSE_VER_STRING "WI-V4.0.0.2411"
-#define FILE_VER_NUMBER 4, 0, 0, 2411
+#define PRODUCT_VER_STRING "4.0.0.2416"
+#define FILE_VER_STRING "WI-V4.0.0.2416"
+#define LICENSE_VER_STRING "WI-V4.0.0.2416"
+#define FILE_VER_NUMBER 4, 0, 0, 2416
 #define FB_MAJOR_VER "4"
 #define FB_MINOR_VER "0"
 #define FB_REV_NO "0"
-#define FB_BUILD_NO "2411"
+#define FB_BUILD_NO "2416"
 #define FB_BUILD_TYPE "V"
 #define FB_BUILD_SUFFIX "Firebird 4.0 Release Candidate 1"
--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -9,7 +9,7 @@ BuildType=V
 MajorVer=4
 MinorVer=0
 RevNo=0
-BuildNum=2411
+BuildNum=2416
 
 NowAt=`pwd`
 cd `dirname $0`
