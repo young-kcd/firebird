bdbacfe08c91b6a67f8fd20f28385738dc4220f9
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2021-08-22 11:06:24 -0300

    Add noreturn attribute to raise functions.

--- src/include/fb_exception.h
+++ src/include/fb_exception.h
@@ -102,9 +102,9 @@ public:
 
 	const ISC_STATUS* value() const throw() { return m_status_vector; }
 
-	static void raise(const ISC_STATUS *status_vector);
-	static void raise(const Arg::StatusVector& statusVector);
-	static void raise(const IStatus* status);
+	[[noreturn]] static void raise(const ISC_STATUS* status_vector);
+	[[noreturn]] static void raise(const Arg::StatusVector& statusVector);
+	[[noreturn]] static void raise(const IStatus* status);
 
 protected:
 	// Create exception with undefined status vector, this constructor allows
