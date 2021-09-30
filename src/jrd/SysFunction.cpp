f384412ae26b2dbcc378048a788a9e34d51b0059
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2021-09-30 10:10:38 -0300

    Postfix for #6987 - DATEDIFF does not support fractional value for MILLISECOND.

--- src/jrd/SysFunction.cpp
+++ src/jrd/SysFunction.cpp
@@ -3985,7 +3985,7 @@ dsc* evlDateDiff(thread_db* tdbb, const SysFunction* function, const NestValueAr
 
 		case blr_extract_millisecond:
 			result = (SINT64) ONE_DAY *
-				(timestamp2.value().timestamp_date - timestamp1.value().timestamp_date) * 1000;
+				(timestamp2.value().timestamp_date - timestamp1.value().timestamp_date) * ISC_TIME_SECONDS_PRECISION;
 			result += (SINT64) timestamp2.value().timestamp_time - (SINT64) timestamp1.value().timestamp_time;
 			scale = ISC_TIME_SECONDS_PRECISION_SCALE + 3;
 			break;
