f0f42765a56bc680094fff708be48bc34d4c8483
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2021-12-07 07:10:40 -0300

    Docker builder for Linux.

---
+++ builds/docker/linux/i586-x86_64/scripts/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# FIXME: Resolve this with a configure option.
+sed -i 's/AC_CHECK_LIB(atomic, main)/AC_CHECK_LIB(:libatomic.a, main)/g' configure.ac
+
+./autogen.sh --build=$BUILD_ARCH --prefix=/opt/firebird --enable-binreloc --with-builtin-tomcrypt --with-termlib=:libncurses.a
+make -j${CPUCOUNT}
+make dist
