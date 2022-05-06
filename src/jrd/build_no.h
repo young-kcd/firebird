917e88126e0c4c2ba0528a2e7d922b484a35f1ab
firebirds <>
2022-05-06 00:06:51 +0000

    increment build number

--- src/jrd/build_no.h
+++ src/jrd/build_no.h
@@ -3,16 +3,16 @@
                *** DO NOT EDIT ***
   TO CHANGE ANY INFORMATION IN HERE PLEASE
   EDIT src/misc/writeBuildNum.sh
-  FORMAL BUILD NUMBER:489
+  FORMAL BUILD NUMBER:491
 */
 
-#define PRODUCT_VER_STRING "5.0.0.489"
-#define FILE_VER_STRING "WI-T5.0.0.489"
-#define LICENSE_VER_STRING "WI-T5.0.0.489"
-#define FILE_VER_NUMBER 5, 0, 0, 489
+#define PRODUCT_VER_STRING "5.0.0.491"
+#define FILE_VER_STRING "WI-T5.0.0.491"
+#define LICENSE_VER_STRING "WI-T5.0.0.491"
+#define FILE_VER_NUMBER 5, 0, 0, 491
 #define FB_MAJOR_VER "5"
 #define FB_MINOR_VER "0"
 #define FB_REV_NO "0"
-#define FB_BUILD_NO "489"
+#define FB_BUILD_NO "491"
 #define FB_BUILD_TYPE "T"
 #define FB_BUILD_SUFFIX "Firebird 5.0 Initial"
