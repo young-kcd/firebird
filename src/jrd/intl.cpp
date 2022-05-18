3d8b3cb305fa48e9266bbe2b55fafb62cd510fd9
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2022-05-18 18:06:22 -0300

    Postfix for #7179 - Wrong error message - "string right truncation. expected length 30, actual 30".

--- src/jrd/intl.cpp
+++ src/jrd/intl.cpp
@@ -780,7 +780,7 @@ CsConvert INTL_convert_lookup(thread_db* tdbb, CHARSET_ID to_cs, CHARSET_ID from
 }
 
 
-int INTL_convert_string(dsc* to, const dsc* from, Firebird::Callbacks* cb)
+void INTL_convert_string(dsc* to, const dsc* from, Firebird::Callbacks* cb)
 {
 /**************************************
  *
@@ -791,19 +791,9 @@ int INTL_convert_string(dsc* to, const dsc* from, Firebird::Callbacks* cb)
  * Functional description
  *      Convert a string from one type to another
  *
- * RETURNS:
- *      0 if no error in conversion
- *      non-zero otherwise.
- *      CVC: Unfortunately, this function puts the source in the 2nd param,
- *      as opposed to the CVT routines, so const helps mitigating coding mistakes.
- *
  **************************************/
 
-	// Note: This function is called from outside the engine as
-	// well as inside - we likely can't get rid of JRD_get_thread_data here
-	thread_db* tdbb = JRD_get_thread_data();
-	if (tdbb == NULL)			// are we in the Engine?
-		return (1);				// no, then can't access intl gah
+	const auto tdbb = JRD_get_thread_data();
 
 	fb_assert(to != NULL);
 	fb_assert(from != NULL);
@@ -813,7 +803,6 @@ int INTL_convert_string(dsc* to, const dsc* from, Firebird::Callbacks* cb)
 	const CHARSET_ID to_cs = INTL_charset(tdbb, INTL_TTYPE(to));
 
 	UCHAR* p = to->dsc_address;
-	const UCHAR* start = p;
 
 	// Must convert dtype(cstring,text,vary) and ttype(ascii,binary,..intl..)
 
@@ -823,135 +812,94 @@ int INTL_convert_string(dsc* to, const dsc* from, Firebird::Callbacks* cb)
 		tdbb->getAttachment()->att_dec_status, cb->err);
 
 	const ULONG to_size = TEXT_LEN(to);
-	ULONG from_fill, to_fill;
 
 	const UCHAR* q = from_ptr;
 	CharSet* const toCharSet = INTL_charset_lookup(tdbb, to_cs);
-	ULONG toLength;
 
-	switch (to->dsc_dtype)
+	UCHAR* const to_ptr = to->dsc_dtype == dtype_varying ?
+		reinterpret_cast<UCHAR*>(((vary*) p)->vary_string) :
+		p;
+
+	ULONG to_fill;
+
+	if (from_cs != to_cs && to_cs != CS_BINARY && to_cs != CS_NONE && from_cs != CS_NONE)
 	{
-	case dtype_text:
-		if (from_cs != to_cs && to_cs != CS_BINARY && to_cs != CS_NONE && from_cs != CS_NONE)
-		{
-			const ULONG to_len = INTL_convert_bytes(tdbb, to_cs, to->dsc_address, to_size,
-										from_cs, from_ptr, from_len, cb->err);
-			toLength = to_len;
-			to_fill = to_size - to_len;
-			from_fill = 0;		// Convert_bytes handles source truncation
-			p += to_len;
-		}
-		else
-		{
-			// binary string can always be converted TO by byte-copy
+		ULONG to_len;
 
-			ULONG to_len = MIN(from_len, to_size);
-			if (!toCharSet->wellFormed(to_len, q))
-				cb->err(Arg::Gds(isc_malformed_string));
-			toLength = to_len;
-			from_fill = from_len - to_len;
-			to_fill = to_size - to_len;
-			if (to_len)
+		try
+		{
+			to_len = INTL_convert_bytes(tdbb, to_cs, to_ptr, to_size, from_cs, from_ptr, from_len, cb->err);
+		}
+		catch (const status_exception& e)
+		{
+			const auto status = e.value();
+
+			if (status[0] == isc_arg_gds &&
+				status[1] == isc_arith_except &&
+				status[2] == isc_arg_gds &&
+				status[3] == isc_string_truncation &&
+				status[4] == isc_arg_gds &&
+				status[5] == isc_trunc_limits)
 			{
-				do
-				{
-					*p++ = *q++;
-				} while (--to_len);
+				const auto fromCharSet = INTL_charset_lookup(tdbb, from_cs);
+
+				// This should throw another exception with better information.
+				cb->validateLength(fromCharSet, from_cs, from_len, from_ptr,
+					to_size / toCharSet->maxBytesPerChar() * fromCharSet->maxBytesPerChar());
 			}
+
+			throw;
 		}
 
-		if (to_fill > 0)
-			pad_spaces(tdbb, to_cs, p, to_fill);
-		break;
+		to_len = cb->validateLength(toCharSet, to_cs, to_len, to_ptr, to_size);
 
-	case dtype_cstring:
-		if (from_cs != to_cs && to_cs != CS_BINARY && to_cs != CS_NONE && from_cs != CS_NONE)
+		switch (to->dsc_dtype)
 		{
-			const ULONG to_len = INTL_convert_bytes(tdbb, to_cs, to->dsc_address, to_size,
-										from_cs, from_ptr, from_len, cb->err);
-			toLength = to_len;
-			to->dsc_address[to_len] = 0;
-			from_fill = 0;		// Convert_bytes handles source truncation
+			case dtype_text:
+				to_fill = to_size - to_len;
+				p += to_len;
+				break;
+
+			case dtype_cstring:
+				p[to_len] = 0;
+				break;
+
+			case dtype_varying:
+				((vary*) p)->vary_length = to_len;
+				break;
 		}
-		else
+	}
+	else
+	{
+		// binary string can always be converted TO by byte-copy
+
+		if (!toCharSet->wellFormed(from_len, q))
+			cb->err(Arg::Gds(isc_malformed_string));
+
+		ULONG to_len = cb->validateLength(toCharSet, to_cs, from_len, q, to_size);
+
+		to_fill = to_size - to_len;
+
+		if (to->dsc_dtype == dtype_varying)
 		{
-			// binary string can always be converted TO by byte-copy
-
-			ULONG to_len = MIN(from_len, to_size);
-			if (!toCharSet->wellFormed(to_len, q))
-				cb->err(Arg::Gds(isc_malformed_string));
-			toLength = to_len;
-			from_fill = from_len - to_len;
-			if (to_len)
-			{
-				do
-				{
-					*p++ = *q++;
-				} while (--to_len);
-			}
-			*p = 0;
-		}
-		break;
-
-	case dtype_varying:
-		if (from_cs != to_cs && to_cs != CS_BINARY && to_cs != CS_NONE && from_cs != CS_NONE)
-		{
-			UCHAR* vstr = reinterpret_cast<UCHAR*>(((vary*) to->dsc_address)->vary_string);
-			start = vstr;
-			ULONG to_len = INTL_convert_bytes(tdbb, to_cs, vstr,
-										to_size, from_cs, from_ptr, from_len, cb->err);
-
-			to_len = cb->validateLength(toCharSet, to_cs, to_len, vstr, to_size);
-
-			toLength = to_len;
-			((vary*) to->dsc_address)->vary_length = to_len;
-			from_fill = 0;		// Convert_bytes handles source truncation
-		}
-		else
-		{
-			// binary string can always be converted TO by byte-copy
-			ULONG to_len = MIN(from_len, to_size);
-			if (!toCharSet->wellFormed(to_len, q))
-				cb->err(Arg::Gds(isc_malformed_string));
-
-			to_len = cb->validateLength(toCharSet, to_cs, to_len, q, to_size);
-
-			toLength = to_len;
-			from_fill = from_len - to_len;
 			((vary*) p)->vary_length = to_len;
-			start = p = reinterpret_cast<UCHAR*>(((vary*) p)->vary_string);
-			if (to_len)
-			{
-				do
-				{
-					*p++ = *q++;
-				} while (--to_len);
-			}
+			p = to_ptr;
 		}
-		break;
-	}
 
-	CharSet* const fromCharSet = INTL_charset_lookup(tdbb, from_cs);
-	const ULONG src_len = fromCharSet->length(from_len, from_ptr, false);
-	const ULONG dest_len  = (ULONG) to_size / toCharSet->maxBytesPerChar();
-
-	if (toCharSet->isMultiByte() && src_len > dest_len)
-	{
-		cb->err(Arg::Gds(isc_arith_except) << Arg::Gds(isc_string_truncation) <<
-			Arg::Gds(isc_trunc_limits) << Arg::Num(dest_len) << Arg::Num(src_len));
-	}
-
-	if (from_fill)
-	{
-		// Make sure remaining characters on From string are spaces
-		if (!allSpaces(INTL_charset_lookup(tdbb, from_cs), q, from_fill, 0))
+		if (to_len)
 		{
-			cb->err(Arg::Gds(isc_arith_except) << Arg::Gds(isc_string_truncation) <<
-				Arg::Gds(isc_trunc_limits) << Arg::Num(dest_len) << Arg::Num(src_len));
+			do
+			{
+				*p++ = *q++;
+			} while (--to_len);
 		}
+
+		if (to->dsc_dtype == dtype_cstring)
+			*p = 0;
 	}
 
-	return 0;
+	if (to->dsc_dtype == dtype_text && to_fill > 0)
+		pad_spaces(tdbb, to_cs, p, to_fill);
 }
 
 
