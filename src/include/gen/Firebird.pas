38ddd6b646b024e9ed04e2ac46dec96286c777e8
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2022-07-14 22:10:00 -0300

    Update generated file.

--- src/include/gen/Firebird.pas
+++ src/include/gen/Firebird.pas
@@ -5174,6 +5174,8 @@ const
 	 isc_bad_loctab_num = 335545277;
 	 isc_quoted_str_bad = 335545278;
 	 isc_quoted_str_miss = 335545279;
+	 isc_wrong_shmem_ver = 335545280;
+	 isc_wrong_shmem_bitness = 335545281;
 	 isc_gfix_db_name = 335740929;
 	 isc_gfix_invalid_sw = 335740930;
 	 isc_gfix_incmp_sw = 335740932;
