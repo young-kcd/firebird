96931200340c8e0abcd3529b8e443d5ab62adfa5
Piotr Kubaj <pkubaj@FreeBSD.org>
2022-07-28 14:33:34 +0200

    Add support for FreeBSD/powerpc64le

--- src/include/firebird/impl/inf_pub.h
+++ src/include/firebird/impl/inf_pub.h
@@ -298,6 +298,8 @@ enum  info_db_implementations
 	isc_info_db_impl_linux_m68k = 87,
 	isc_info_db_impl_linux_riscv64 = 88,
 
+	isc_info_db_impl_freebsd_ppc64el = 89,
+
 	isc_info_db_impl_last_value   // Leave this LAST!
 };
 
