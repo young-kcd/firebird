0117da2839d894af729f6daa89f1e87bd5cc3e61
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2022-08-07 21:14:52 -0300

    Upload zip generated in builds/install_images in GitHub actions.

--- builds/docker/windows/scripts/build-x86.bat
+++ builds/docker/windows/scripts/build-x86.bat
@@ -8,9 +8,8 @@ set FB_OUTPUT_SUFFIX=win32
 xcopy /h /e /i /q C:\firebird C:\firebird-build
 cd /d C:\firebird-build\builds\win32
 
-REM call run_all.bat
 call run_all.bat
 
 call run_tests.bat
 
-xcopy /h /e /i /q C:\firebird-build\output_%FB_OUTPUT_SUFFIX%\* C:\firebird-out
+copy C:\firebird-build\builds\install_images\* C:\firebird\builds\install_images
