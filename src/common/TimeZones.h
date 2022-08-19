6bbca398b811666826d2ab465334bfd159207c15
asfernandes <asfernandes@users.noreply.github.com>
2022-08-19 11:14:01 +0000

    Update tzdata to version 2022b.

--- src/common/TimeZones.h
+++ src/common/TimeZones.h
@@ -1,6 +1,6 @@
 // The content of this file is generated with help of update-ids utility Do not edit.
 
-static const char* BUILTIN_TIME_ZONE_VERSION = "2022a";
+static const char* BUILTIN_TIME_ZONE_VERSION = "2022b";
 
 // Do not change order of items in this array! The index corresponds to a TimeZone ID, which must be fixed!
 static const char* BUILTIN_TIME_ZONE_LIST[] = {
@@ -638,5 +638,6 @@ static const char* BUILTIN_TIME_ZONE_LIST[] = {
 	"Zulu",	// fb_tzid_zulu - 64904
 	"America/Nuuk",	// fb_tzid_america_nuuk - 64903
 	"Asia/Qostanay",	// fb_tzid_asia_qostanay - 64902
-	"Pacific/Kanton"	// fb_tzid_pacific_kanton - 64901
+	"Pacific/Kanton",	// fb_tzid_pacific_kanton - 64901
+	"Europe/Kyiv"	// fb_tzid_europe_kyiv - 64900
 };
--- src/include/firebird/TimeZones.h
+++ src/include/firebird/TimeZones.h
@@ -638,5 +638,6 @@
 #define fb_tzid_america_nuuk                         64903 /* America/Nuuk */
 #define fb_tzid_asia_qostanay                        64902 /* Asia/Qostanay */
 #define fb_tzid_pacific_kanton                       64901 /* Pacific/Kanton */
+#define fb_tzid_europe_kyiv                          64900 /* Europe/Kyiv */
 
 #endif /* FIREBIRD_TIME_ZONES_H */
