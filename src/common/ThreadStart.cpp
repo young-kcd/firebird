523300236e61d59171f93a15dfc32f622eb181cd
Vlad Khorsun <hvlad@users.sourceforge.net>
2022-09-20 14:42:45 +0300

    Avoid too early kill of probably working (not hung) thread.

--- src/common/ThreadStart.cpp
+++ src/common/ThreadStart.cpp
@@ -345,7 +345,7 @@ void Thread::waitForCompletion(Handle& handle)
 	// exiting, OS notifies every DLL about it, and acquires loader lock. In such
 	// scenario waiting on thread handle will never succeed.
 	if (!Firebird::dDllUnloadTID) {
-		WaitForSingleObject(handle, 500);
+		WaitForSingleObject(handle, 10000);
 	}
 	CloseHandle(handle);
 	handle = 0;
