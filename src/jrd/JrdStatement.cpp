39ce8ba80ca64c9351343b5df54f11b8919f8ef9
AlexPeshkoff <alexander.peshkoff@gmail.com>
2022-11-02 20:17:19 +0300

    Fixed #7370: Segfault under OOM conditions

--- src/jrd/JrdStatement.cpp
+++ src/jrd/JrdStatement.cpp
@@ -397,18 +397,17 @@ jrd_req* JrdStatement::getRequest(thread_db* tdbb, USHORT level)
 	if (level < requests.getCount() && requests[level])
 		return requests[level];
 
-	requests.grow(level + 1);
-
+	// Create the request.
 	MemoryStats* const parentStats = (flags & FLAG_INTERNAL) ?
 		&dbb->dbb_memory_stats : &attachment->att_memory_stats;
 
-	// Create the request.
-	jrd_req* const request = FB_NEW_POOL(*pool) jrd_req(attachment, this, parentStats);
+	AutoPtr<jrd_req> request(FB_NEW_POOL(*pool) jrd_req(attachment, this, parentStats));
 	request->setRequestId(dbb->generateStatementId());
 
+	requests.grow(level + 1);
 	requests[level] = request;
 
-	return request;
+	return request.release();
 }
 
 // Check that we have enough rights to access all resources this request touches including
@@ -640,7 +639,10 @@ void JrdStatement::release(thread_db* tdbb)
 	}
 
 	for (jrd_req** instance = requests.begin(); instance != requests.end(); ++instance)
-		EXE_release(tdbb, *instance);
+	{
+		if (*instance)
+			EXE_release(tdbb, *instance);
+	}
 
 	sqlText = NULL;
 
