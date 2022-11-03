c8b74dcbf860bcf1706129796509895fc63d15bf
AlexPeshkoff <alexander.peshkoff@gmail.com>
2022-11-03 17:53:27 +0300

    Fixed #7357: lock manager error when connect a Firebird 3.0 database more than once using Classic/SuperClassic Firebird 4.0 service

--- src/yvalve/why.cpp
+++ src/yvalve/why.cpp
@@ -6390,6 +6390,7 @@ YAttachment* Dispatcher::attachOrCreateDatabase(CheckStatusWrapper* status, bool
 			case isc_lock_dir_access:
 			case isc_no_priv:
 			case isc_wrong_ods:
+			case isc_wrong_shmem_ver:
 				currentStatus = &tempCheckStatusWrapper;
 				// fall down...
 			case isc_unavailable:
